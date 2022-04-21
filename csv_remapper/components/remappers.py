8a1635ad615ab73c36415dbfb23fec1dbff071d9
andrejp <andrej.perfilov@gmail.com>
2022-04-21 20:24:25 +0100

    move alias lookup logic to a separate function

--- csv_remapper/components/remappers.py
+++ csv_remapper/components/remappers.py
@@ -10,7 +10,9 @@ from csv_remapper.components import (
 from csv_remapper.constants import *
 
 
-def remap_csv_file(csv_file, out_template_name, dir_handler):  # type: (str, str, io_handlers.AppDirectoryHandler) -> dict
+def remap_csv_file(csv_file, out_template_name, dir_handler):
+    # type: (str, str, io_handlers.AppDirectoryHandler) -> dict
+
     input_csv_data = io_handlers.CsvFileHandler.read(csv_file)
     alias_data = _get_alias_data(out_template_name, dir_handler)
     out_temp_files = dir_handler.get_template_files(out_template_name, OUTPUT_K)  # cache output template files
@@ -23,64 +25,73 @@ def remap_csv_file(csv_file, out_template_name, dir_handler):  # type: (str, str
     output_data[DATA_K] = []  # clear output data before populating it
 
     for row in input_csv_data[DATA_K]:
-        skip_row = False
         output_row = {key: '' for key in output_data[HEADERS_K]}  # create empty data row
         for mapping in mapping_data[MAPPINGS_K]:
-            if not skip_row:
+            if output_row:
                 if mapping[SOURCE_COLUMN_K] not in row:
                     raise Exception(f'Invalid Mapping found: {mapping}')
+
                 cell_data = row[mapping[SOURCE_COLUMN_K]]
                 if cell_data.strip() and mapping[TARGET_COLUMN_K] in output_data[HEADERS_K]:
                     # only insert data that is present in the output template header
                     output_row[mapping[TARGET_COLUMN_K]] = _convert_value(
-                        cell_data, mapping[IN_DATA_K], mapping[OUT_DATA_K]
+                        cell_data,
+                        mapping[IN_DATA_K],
+                        mapping[OUT_DATA_K]
                     )
+
+                # try to interpret the incoming data based on the alias lookup mode
                 if mapping[TARGET_COLUMN_K] == ALIAS_FIELD_NAME:
-                    # try to interpret the incoming data based on the alias lookup mode
-                    match = None
                     alias_name = mapping[NAME_K].split(MAPPING_SEPARATOR)[-1]
                     if alias_name not in alias_data:
                         raise Exception('Please ensure a valid Output Template is selected')
-                    data = alias_data[alias_name]
-                    for a_data in data['csv'][DATA_K]:  # type: dict
-                        # go through alias data row by row and return as soon as first match is found
-                        if a_data[ALIAS_FIELD_NAME].strip() and cell_data.strip():
-                            alias_strings = a_data[ALIAS_FIELD_NAME].split(ALIAS_DATA_SEPARATOR)
-                            alias_strings = [s.lower() for s in alias_strings]
-                            if data['json'][LOOKUP_MODE_K] == datatypes.LookupModes.MATCH_TEXT:
-                                # try to find one of the aliases in 'cell_data'
-                                if any([s in cell_data.lower() for s in alias_strings]):
-                                    match = a_data
-                                    break
-                            elif data['json'][LOOKUP_MODE_K] == datatypes.LookupModes.MATCH_COL:
-                                # multiple mappings expected; choose the first one with data in it
-                                if any([s in mapping[SOURCE_COLUMN_K].lower() for s in alias_strings]):
-                                    match = a_data
-                                    break
-                            elif data['json'][LOOKUP_MODE_K] == datatypes.LookupModes.OMIT_ROW:
-                                # omit the row entirely if one of the aliases is found in 'cell_data'
-                                if any([s in cell_data.lower() for s in alias_strings]):
-                                    output_row = None
-                                    skip_row = True
-                                    break
-                            elif data['json'][LOOKUP_MODE_K] == datatypes.LookupModes.REGEX:
-                                # a single regular expression is expected in the Alias Data column
-                                # choose the first one that returns a result
-                                regex = a_data[ALIAS_FIELD_NAME]
-                                result = _extract_substring(cell_data, regex)
-                                if result:
-                                    copy_data = a_data.copy()
-                                    copy_data[ALIAS_FIELD_NAME] = result
-                                    match = copy_data
-                                    break
+
+                    match, output_row = _find_match(cell_data, alias_data[alias_name], mapping, output_row)
+
                     if match:
-                        for a_mapping in data['json'][MAPPINGS_K]:
+                        for a_mapping in alias_data[alias_name]['json'][MAPPINGS_K]:
                             output_row[a_mapping[TARGET_COLUMN_K]] = _convert_value(
-                                match[a_mapping[SOURCE_COLUMN_K]], a_mapping[IN_DATA_K], a_mapping[OUT_DATA_K]
+                                match[a_mapping[SOURCE_COLUMN_K]],
+                                a_mapping[IN_DATA_K],
+                                a_mapping[OUT_DATA_K]
                             )
         if output_row:
             output_data[DATA_K].append(output_row)
     return output_data
+
+
+def _find_match(cell_data, data, mapping, output_row):  # type: (str, dict, dict, dict) -> tuple[dict, dict]
+    """ Go through alias data row by row and return as soon as first match is found """
+    for a_data in data['csv'][DATA_K]:  # type: dict
+        if a_data[ALIAS_FIELD_NAME].strip() and cell_data.strip():
+            alias_strings = a_data[ALIAS_FIELD_NAME].split(ALIAS_DATA_SEPARATOR)
+            alias_strings = [s.lower() for s in alias_strings]
+
+            if data['json'][LOOKUP_MODE_K] == datatypes.LookupModes.MATCH_TEXT:
+                # try to find one of the aliases in 'cell_data'
+                if any([s in cell_data.lower() for s in alias_strings]):
+                    return a_data, output_row
+
+            elif data['json'][LOOKUP_MODE_K] == datatypes.LookupModes.MATCH_COL:
+                # multiple mappings expected; choose the first one with data in it
+                if any([s in mapping[SOURCE_COLUMN_K].lower() for s in alias_strings]):
+                    return a_data, output_row
+
+            elif data['json'][LOOKUP_MODE_K] == datatypes.LookupModes.OMIT_ROW:
+                # omit the row entirely if one of the aliases is found in 'cell_data'
+                if any([s in cell_data.lower() for s in alias_strings]):
+                    return {}, {}
+
+            elif data['json'][LOOKUP_MODE_K] == datatypes.LookupModes.REGEX:
+                # a single regular expression is expected in the Alias Data column
+                # choose the first one that returns a result
+                regex = a_data[ALIAS_FIELD_NAME]
+                result = _extract_substring(cell_data, regex)
+                if result:
+                    copy_data = a_data.copy()
+                    copy_data[ALIAS_FIELD_NAME] = result
+                    return copy_data, output_row
+    return {}, {}
 
 
 def _get_input_template_name(input_csv_file, input_csv_data, out_template_name, dir_handler):
